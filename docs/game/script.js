a0ebd7e0bba6871332ccd35d20a2f84b3e017a36
dudushy <eduardotalarico1337@gmail.com>
2024-02-18 19:31:38 -0300

    Working on game page (loadSettings)

--- docs/game/script.js
+++ docs/game/script.js
@@ -1,2 +1,25 @@
 const TITLE = "lefante/game";
 console.log(`[${TITLE}] init`);
+
+const settingsDefault = {
+  "theme": "light",
+  "pairs": 6,
+  "lifes": 3,
+}
+
+let settings = settingsDefault;
+
+const lifeDisplay = document.querySelector(".lifeDisplay #life-count");
+console.log(`[${TITLE}] lifeDisplay`, lifeDisplay);
+
+loadSettings();
+
+function loadSettings() {
+  console.log(`[${TITLE}#loadSettings] (BEFORE) settings`, settings);
+
+  settings = JSON.parse(localStorage.getItem("settings")) || settingsDefault;
+
+  console.log(`[${TITLE}#loadSettings] (AFTER) settings`, settings);
+
+  lifeDisplay.textContent = settings.lifes;
+}
